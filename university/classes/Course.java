832334ece23942099714bacc340c75c0879414ad
akiranosei <158202219+akiranosei@users.noreply.github.com>
2026-05-17 20:26:25 +0500

    Add import for university.model package

--- university/classes/Course.java
+++ university/classes/Course.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import university.model.*;
 
 public class Course implements Serializable {
     private static final long serialVersionUID = 1L;
