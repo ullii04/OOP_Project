02b3e18612e25be471d801f39e5832fbefad3985
akiranosei <158202219+akiranosei@users.noreply.github.com>
2026-05-17 19:20:54 +0500

    add exception

--- university/exceptions/MaxCreditsExceededException.java
+++ university/exceptions/MaxCreditsExceededException.java
@@ -1,5 +1,11 @@
 package university.exceptions;
-
 public class MaxCreditsExceededException extends Exception {
-    public MaxCreditsExceededException(String message) { super(message); }
+    public MaxCreditsExceededException(String message) {
+        super(message); }
+
+    public MaxCreditsExceededException(int currentCredits, int addedCredits, int maxCredits) {
+        super("Credit limit exceeded: current=" + currentCredits
+                + ", added=" + addedCredits
+                + ", max=" + maxCredits);
+    }
 }
